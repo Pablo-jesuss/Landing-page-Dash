fa3fd3cd703d37616de2c2722772902351ad2090
Pablo-jesuss <pablo.programador26@gmail.com>
2024-04-22 15:22:05 -0300

    header

--- Painel/src/App.jsx
+++ Painel/src/App.jsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
 
 import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import Home from "./Home";
 
 function App() {
-  return <div className="App-container"></div>;
+  return (
+    <div className="App-container">
+      <Header />
+      <Sidebar />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
--- Painel/src/Sidebar.jsx
+++ Painel/src/Sidebar.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Sidebar = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Sidebar</h1>
+    </div>
+  );
 };
 
 export default Sidebar;
