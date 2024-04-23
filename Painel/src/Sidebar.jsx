801f17d42006727ad4ebe30fb985d47d0ba04228
Pablo-jesuss <pablo.programador26@gmail.com>
2024-04-23 09:39:26 -0300

    iconSidebar

--- Painel/src/Sidebar.jsx
+++ Painel/src/Sidebar.jsx
@@ -1,5 +1,14 @@
 import React from "react";
-import { BsCart3 } from "react-icons/bs";
+import {
+  BsCart3,
+  BsGrid1X2Fill,
+  BsFillArchiveFill,
+  BsFillGrid3X3GapFill,
+  BsPeopleFill,
+  BsListCheck,
+  BsMenuButtonWideFill,
+  BsFillGearFill,
+} from "react-icons/bs";
 
 const Sidebar = () => {
   return (
@@ -14,43 +23,43 @@ const Sidebar = () => {
       <ul className="sidebar-list">
         <li className="sidebar-list-item">
           <a href="">
-            <BsCart3 className="icon" /> Dashboard
+            <BsGrid1X2Fill className="icon" /> Dashboard
           </a>
         </li>
 
         <li className="sidebar-list-item">
           <a href="">
-            <BsCart3 className="icon" /> Products
+            <BsFillArchiveFill className="icon" /> Products
           </a>
         </li>
 
         <li className="sidebar-list-item">
           <a href="">
-            <BsCart3 className="icon" /> Categories
+            <BsFillGrid3X3GapFill className="icon" /> Categories
           </a>
         </li>
 
         <li className="sidebar-list-item">
           <a href="">
-            <BsCart3 className="icon" /> Customers
+            <BsPeopleFill className="icon" /> Customers
           </a>
         </li>
 
         <li className="sidebar-list-item">
           <a href="">
-            <BsCart3 className="icon" /> Inventory
+            <BsListCheck className="icon" /> Inventory
           </a>
         </li>
 
         <li className="sidebar-list-item">
           <a href="">
-            <BsCart3 className="icon" /> Reports
+            <BsMenuButtonWideFill className="icon" /> Reports
           </a>
         </li>
 
         <li className="sidebar-list-item">
           <a href="">
-            <BsCart3 className="icon" /> Setting
+            <BsFillGearFill className="icon" /> Setting
           </a>
         </li>
       </ul>
