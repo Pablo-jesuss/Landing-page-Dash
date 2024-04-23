cb66a0955d3edbe40e3d9efac594ba460daeb1f1
Pablo-jesuss <pablo.programador26@gmail.com>
2024-04-23 09:34:04 -0300

    sidebarList

--- Painel/src/Sidebar.jsx
+++ Painel/src/Sidebar.jsx
@@ -10,6 +10,50 @@ const Sidebar = () => {
         </div>
         <span className="icon close-icon">X</span>
       </div>
+
+      <ul className="sidebar-list">
+        <li className="sidebar-list-item">
+          <a href="">
+            <BsCart3 className="icon" /> Dashboard
+          </a>
+        </li>
+
+        <li className="sidebar-list-item">
+          <a href="">
+            <BsCart3 className="icon" /> Products
+          </a>
+        </li>
+
+        <li className="sidebar-list-item">
+          <a href="">
+            <BsCart3 className="icon" /> Categories
+          </a>
+        </li>
+
+        <li className="sidebar-list-item">
+          <a href="">
+            <BsCart3 className="icon" /> Customers
+          </a>
+        </li>
+
+        <li className="sidebar-list-item">
+          <a href="">
+            <BsCart3 className="icon" /> Inventory
+          </a>
+        </li>
+
+        <li className="sidebar-list-item">
+          <a href="">
+            <BsCart3 className="icon" /> Reports
+          </a>
+        </li>
+
+        <li className="sidebar-list-item">
+          <a href="">
+            <BsCart3 className="icon" /> Setting
+          </a>
+        </li>
+      </ul>
     </aside>
   );
 };
