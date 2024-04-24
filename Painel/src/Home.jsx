ec00e3040a4b854035b0c2167dc08a1a263fc1c0
Pablo-jesuss <pablo.programador26@gmail.com>
2024-04-24 14:55:38 -0300

    home

--- Painel/src/Home.jsx
+++ Painel/src/Home.jsx
@@ -2,9 +2,19 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <main className="main-container">
+      <div className="main-title">
+        <h3>DASHBOARD</h3>
+      </div>
+
+      <div className="main-cards">
+        <div className="card-inner">
+          <h3>PRODUCTS</h3>
+          <></>
+        </div>
+        <h1>300</h1>
+      </div>
+    </main>
   );
 };
 
