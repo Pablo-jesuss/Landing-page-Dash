e76c772820d587d23687295f784c4f08cbe80bd7
Pablo-jesuss <pablo.programador26@gmail.com>
2024-04-24 15:24:51 -0300

    line

--- Painel/src/Home.jsx
+++ Painel/src/Home.jsx
@@ -14,6 +14,9 @@ import {
   CartesianGrid,
   Tooltip,
   ResponsiveContainer,
+  LineChart,
+  Line,
+  Legend,
 } from "recharts";
 
 function Home() {
@@ -125,6 +128,33 @@ function Home() {
                 fill="#8884d8"
               />
             </AreaChart>
+          </ResponsiveContainer>
+
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart
+              width={500}
+              height={300}
+              data={data}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line
+                type="monotone"
+                dataKey="pv"
+                stroke="#8884d8"
+                activeDot={{ r: 8 }}
+              />
+              <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+            </LineChart>
           </ResponsiveContainer>
         </div>
       </div>
