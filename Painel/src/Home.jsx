032d1451fbbe3157660269343286f84537dbb939
Pablo-jesuss <pablo.programador26@gmail.com>
2024-04-24 15:09:42 -0300

    cards

--- Painel/src/Home.jsx
+++ Painel/src/Home.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import {
+  BsFillArchiveFill,
+  BsFillGrid3X3GapFill,
+  BsPeopleFill,
+  BsFillBellFill,
+} from "react-icons/bs";
 
 const Home = () => {
   return (
@@ -8,11 +14,37 @@ const Home = () => {
       </div>
 
       <div className="main-cards">
-        <div className="card-inner">
-          <h3>PRODUCTS</h3>
-          <></>
+        <div className="card">
+          <div className="card-inner">
+            <h3>PRODUCTS</h3>
+            <BsFillArchiveFill className="card-icon" />
+          </div>
+          <h1>300</h1>
         </div>
-        <h1>300</h1>
+
+        <div className="card">
+          <div className="card-inner">
+            <h3>CATEGORIES</h3>
+            <BsFillGrid3X3GapFill className="card-icon" />
+          </div>
+          <h1>12</h1>
+        </div>
+
+        <div className="card">
+          <div className="card-inner">
+            <h3>CUSTOMERS</h3>
+            <BsPeopleFill className="card-icon" />
+          </div>
+          <h1>33</h1>
+        </div>
+
+        <div className="card">
+          <div className="card-inner">
+            <h3>ALERTS</h3>
+            <BsFillBellFill className="card-icon" />
+          </div>
+          <h1>42</h1>
+        </div>
       </div>
     </main>
   );
